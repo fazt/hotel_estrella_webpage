aa54386ef834b894efdc9fee1c1ebbaa482300b9
FaztJs <fabianncdev@gmail.com>
2016-10-18 11:37:36 +0000

    primer diseño

---
+++ app.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var routes = require('./routes');
+
+const PORT = 3000;
+app.set('port',process.env.PORT || PORT);
+app.set('views',path.join(__dirname,'views'));
+app.engine('.hbs', exphbs({
+  defautlLayout: 'index',
+  partials: path.join(app.get('views'),'partials'),
+  extname:'.hbs'
+}));
+app.set('view engine','.hbs');
+
+app.use('/',routes);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(app.get('port'),() => {
+  console.log(`http://localhost:${app.get('port')}`);
+})
